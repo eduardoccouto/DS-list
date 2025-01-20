3a1b750fc526c04f6a441ff9faf36779fb8efbe9
Eduardo Couto <coutoeduardo858@gmail.com>
2025-01-20 11:19:13 -0300

    first commit

---
+++ src/test/java/com/eduardo/DS_list/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.DS_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
